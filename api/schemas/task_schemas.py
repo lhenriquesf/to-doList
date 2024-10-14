85442b3168cedc23bb082db4037cb09cebc1728e
lhenriquesf <franca.luiz1@hotmail.com>
2024-10-14 00:16:38 -0300

    adicionando validação de tarefa vazia

--- api/schemas/task_schemas.py
+++ api/schemas/task_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 from tortoise.contrib.pydantic import pydantic_model_creator
 from api.models.task_models import Task
@@ -7,8 +7,22 @@ GetTask = pydantic_model_creator(Task, name="Task")
 
 class PostTask(BaseModel):
     task:str = Field(...,max_length=100)
-    done:bool
+    done:bool = Field(default=False)
+
+    @field_validator('task')
+    def check_not_empty(task):
+        if not task or task.strip() == "":
+            raise ValueError("A tarefa não pode estar vazia.")
+        return task
+
+
 
 class PutTask(BaseModel):
     task:Optional[str] = Field(None, max_length=100)
-    done:Optional[bool]
+    done:Optional[bool] = Field(None)
+
+    @field_validator('task')
+    def check_not_empty(task):
+        if not task or task.strip() == "":
+            raise ValueError("A tarefa não pode estar vazia.")
+        return task
