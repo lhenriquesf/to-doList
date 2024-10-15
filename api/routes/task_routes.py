899329e71abe13de6be768db3ae8243ea80a22a0
lhenriquesf <franca.luiz1@hotmail.com>
2024-10-15 11:29:03 -0300

    rm redis do  user_routes

--- api/routes/task_routes.py
+++ api/routes/task_routes.py
@@ -8,21 +8,34 @@ import json
 
 task_router = APIRouter(prefix="/api", tags=["Task"])
 
-@task_router.get("/")
-async def get_task():
-    cached_tasks = redis_client.get("tasks")
-    if cached_tasks:
-        return json.loads(cached_tasks)
-    
-    tasks = await GetTask.from_queryset(Task.all())
-    tasks_json = jsonable_encoder(tasks)
-    redis_client.set("tasks", json.dumps(tasks_json))
-    return tasks
-
 # @task_router.get("/")
 # async def get_task():
+#     cached_tasks = redis_client.get("tasks")
+#     if cached_tasks:
+#         return json.loads(cached_tasks)
+    
 #     tasks = await GetTask.from_queryset(Task.all())
+#     tasks_json = jsonable_encoder(tasks)
+#     redis_client.set("tasks", json.dumps(tasks_json))
 #     return tasks
+
+@task_router.get("/")
+async def get_task():
+    tasks = await GetTask.from_queryset(Task.all())
+    return tasks
+
+
+# @task_router.post("/",dependencies = [Depends(dependency=get_current_user)], status_code=201)
+# async def post_task(body: PostTask):
+#     print("Entrou na rota post_task")
+#     data = body.model_dump(exclude_unset=True)
+#     new_task = await Task.create(**data)
+    
+#     updated_tasks = await GetTask.from_queryset(Task.all())
+#     updated_tasks_json = jsonable_encoder(updated_tasks)
+#     redis_client.set("tasks", json.dumps(updated_tasks_json))
+    
+#     return await GetTask.from_tortoise_orm(new_task)
 
 
 @task_router.post("/",dependencies = [Depends(dependency=get_current_user)], status_code=201)
@@ -31,11 +44,25 @@ async def post_task(body: PostTask):
     data = body.model_dump(exclude_unset=True)
     new_task = await Task.create(**data)
     
-    updated_tasks = await GetTask.from_queryset(Task.all())
-    updated_tasks_json = jsonable_encoder(updated_tasks)
-    redis_client.set("tasks", json.dumps(updated_tasks_json))
-    
     return await GetTask.from_tortoise_orm(new_task)
+
+
+
+# @task_router.put("/{key}", dependencies=[Depends(dependency=get_current_user)])
+# async def update_task(key: int, body: PutTask):
+#     data = body.model_dump(exclude_unset=True)
+#     exists_task = await Task.filter(id=key).exists()
+    
+#     if not exists_task:
+#         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    
+#     await Task.filter(id=key).update(**data)
+
+#     updated_tasks = await GetTask.from_queryset(Task.all())
+#     updated_tasks_json = jsonable_encoder(updated_tasks)
+#     redis_client.set("tasks", json.dumps(updated_tasks_json))
+    
+#     return await GetTask.from_queryset_single(Task.get(id=key))
 
 
 @task_router.put("/{key}", dependencies=[Depends(dependency=get_current_user)])
@@ -47,12 +74,26 @@ async def update_task(key: int, body: PutTask):
         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
     
     await Task.filter(id=key).update(**data)
-
-    updated_tasks = await GetTask.from_queryset(Task.all())
-    updated_tasks_json = jsonable_encoder(updated_tasks)
-    redis_client.set("tasks", json.dumps(updated_tasks_json))
     
     return await GetTask.from_queryset_single(Task.get(id=key))
+
+
+
+
+# @task_router.delete("/{key}", dependencies = [Depends(dependency=get_current_user)])
+# async def delete_task(key: int):
+#     exists_task = await Task.filter(id=key).exists()
+    
+#     if not exists_task:
+#         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+ 
+#     await Task.filter(id=key).delete()
+
+#     updated_tasks = await GetTask.from_queryset(Task.all())
+#     updated_tasks_json = jsonable_encoder(updated_tasks)
+#     redis_client.set("tasks", json.dumps(updated_tasks_json))
+    
+#     return {"message": "Tarefa deletada com sucesso"}
 
 
 @task_router.delete("/{key}", dependencies = [Depends(dependency=get_current_user)])
@@ -63,9 +104,5 @@ async def delete_task(key: int):
         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
  
     await Task.filter(id=key).delete()
-
-    updated_tasks = await GetTask.from_queryset(Task.all())
-    updated_tasks_json = jsonable_encoder(updated_tasks)
-    redis_client.set("tasks", json.dumps(updated_tasks_json))
     
     return {"message": "Tarefa deletada com sucesso"}
