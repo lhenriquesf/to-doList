b2e9e6fd9ec528f6529c1bd3210219f78aca2fe7
lhenriquesf <franca.luiz1@hotmail.com>
2024-10-14 21:19:39 -0300

    atualização do app/main com as rotas e models do user

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 from api.routes.task_routes import task_router
+from api.routes.user_routes import user_router
+
 from tortoise.contrib.fastapi import register_tortoise
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = FastAPI()
+
 app.include_router(task_router)
+app.include_router(user_router)
+
 register_tortoise(
     app=app,
     db_url="sqlite://task.db",
     add_exception_handlers=True,
     generate_schemas=True,
-    modules={"models":["api.models.task_models"]}
+    modules={"models":["api.models.task_models", "api.models.user_models"]}
 )
 
 
