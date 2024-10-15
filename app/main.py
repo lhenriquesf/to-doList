cde6ee668771273adf346ec0e91e3794b716b5f3
lhenriquesf <franca.luiz1@hotmail.com>
2024-10-15 01:58:10 -0300

    att app/main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.routes.task_routes import task_router
 from api.routes.user_routes import user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 from tortoise.contrib.fastapi import register_tortoise
 
@@ -9,6 +10,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://frontend-todo-list-chi.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, PUT, DELETE)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 app.include_router(task_router)
 app.include_router(user_router)
